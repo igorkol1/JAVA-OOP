fd3282b340910d541bd91ac3b63b80cc231b402f
Igor Kołodziejczyk <igor.k.kolodziejczyk@gmail.com>
2019-04-28 09:12:16 +0200

    Rozwiązanie zadań z dzień 1 sekcja a

--- a_Zadania/a_Dzien_1/a_Pierwsza_klasa/Main1.java
+++ a_Zadania/a_Dzien_1/a_Pierwsza_klasa/Main1.java
@@ -4,7 +4,11 @@ package a_Zadania.a_Dzien_1.a_Pierwsza_klasa;
 public class Main1 {
 
     public static void main(String[] args) {
-        
+
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        //System.out.println(cat == dog); //Error:(11, 32) java: incomparable types
 
     }
 }
